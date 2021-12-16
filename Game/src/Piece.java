8a7b1e91dde90129f94bb0e92d2d54f1a07d5f79
Danh4160 <63082166+Danh4160@users.noreply.github.com>
2021-12-15 20:59:26 -0500

    Update Piece.java

--- Game/src/Piece.java
+++ Game/src/Piece.java
@@ -2,25 +2,42 @@ public abstract class Piece {
     private int posX;
     private int posY;
     Side side;
-    boolean isDead;
+    private boolean isDead;
 
+    public Piece(int posX, int posY, Side side) {
+        this.posX = posX;
+        this.posY = posY;
+        this.side = side;
+    }
 
     public void kill() {
         this.isDead = true;
-        this.setX(-1);
-        this.setY(-1);
+        this.posX = -1;
+        this.posY = -1;
+    }
+
+    public void move(int x, int y) {
+        this.posX = x;
+        this.posY = y;
     }
 
     /* Abstract methods to be implemented by the subclass */
-    public abstract void move(int x, int y, boolean isEating);
+    public abstract boolean canMove(int x, int y, boolean isEating);
+    public abstract String getSymbol();
 
     /* Getters for the piece's positions */
-    public int getX() { return this.posX;};
-    public int getY() { return this.posY;};
+    public int getX() { return this.posX; };
+    public int getY() { return this.posY; };
+    public Side getSide() { return this.side; };
+    public boolean getIsDead() { return this.isDead; };
+
 
     /* Setters for the piece's positions */
-    public void setX(int x) { this.posX = -1;};
-    public void setY(int y) { this.posY = -1;};
+    public void setX(int x) { this.posX = -1; };
+    public void setY(int y) { this.posY = -1; };
+    public void setSide(Side side) { this.side = side; };
+    public void setIsDead(boolean isDead) { this.isDead = isDead; };
+
 }
 
 enum Side { WHITE, BLACK }
