a74fb148f0fde0bbc8f89174fcdab58ff85941be
Danh4160 <63082166+Danh4160@users.noreply.github.com>
2021-12-15 20:59:30 -0500

    Create Board.java

---
+++ Game/src/Board.java
@@ -0,0 +1,13 @@
+public class Board {
+    public static int rows = 8;
+    public static int columns = 8;
+    String[][] array;
+
+    public Board() {
+        array = new String[rows][columns];
+
+        // Place all the pieces on the case
+
+    }
+
+}
