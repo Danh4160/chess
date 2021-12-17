3f4d89890a0301c552d2e434d54dd1df3d4520f4
Danh4160 <63082166+Danh4160@users.noreply.github.com>
2021-12-17 00:51:49 -0500

    Rook Template

---
+++ Game/src/Rook.java
@@ -0,0 +1,14 @@
+public class Rook extends Piece {
+
+    public Rook(int x, Side side) {
+        super(x, side == Side.BLACK ? 7 : 0, side);
+    }
+
+    public boolean canMove(int x, int y, boolean isEating) {
+        return false;
+    }
+
+    public String getSymbol() {
+        return this.getSide() == Side.BLACK ? "♜" : "♖";
+    }
+}
